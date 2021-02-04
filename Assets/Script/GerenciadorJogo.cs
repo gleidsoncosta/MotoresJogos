a1f59cf6c5d69cd62eee0fb9aee66a3b5043b31d
gleidsoncosta <gmendes.costa@hotmail.com>
2021-02-04 05:48:51 -0300

    modulo 7 aula 13 - interface do jogo

---
+++ Assets/Script/GerenciadorJogo.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EstadoDoJogo{INICIAL, JOGANDO, PAUSADO};
+public class GerenciadorJogo : MonoBehaviour
+{
+    public EstadoDoJogo estados = EstadoDoJogo.INICIAL;
+    public GameObject[] interfaceInicial;
+    public GameObject gerenciadorObstaculos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void IniciarJogo(){
+        //desativa interface
+        foreach(GameObject elem in interfaceInicial){
+            elem.SetActive(false);
+        }
+
+        gerenciadorObstaculos.SetActive(true);
+
+        //destativa gerenciador
+        Debug.Log("iniciou jogo");
+    }
+}
